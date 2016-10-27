3de17f0805ce8d508ea86f9e7d22dee33b728ba2
f31816 <f31816@gmail.com>
2016-10-28 02:09:15 +0800

    intial draft

---
+++ cdefs/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int test() {
+    return 5566;
+}
+
+int main() {
+    printf("TEST %d\n",test());
+}
--- server.lua
+++ server.lua
@@ -1,1 +1,11 @@
-print "server.lua`"
+package.path = package.path .. ";./libs/?;./libs/?.lua;"
+require("cdefs.test_ffi")
+
+local ffi = require("ffi")
+
+--local C = ffi.load('test')
+
+print "server.lua"
+ffi.C.printf("Hello %s!\n", "world")
+print ("TEST : ",ffi.C.test())
+
